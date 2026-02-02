a5f638811a6903afad2e6835c32fa8bf3d439d3d
KR5T <sakarya1869@gmail.com>
2026-02-02 17:56:57 +0300

    Enemy Animations 1.0

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    public Animator animator;
     public int health = 100;
     bool isDead = false;
 
+    const string HIT_STRING = "Hit";
+
     public void TakeDamage()
     {
+        animator.Play(HIT_STRING, 0, 0f);
         health -= 30;
         if(health <= 0) 
             isDead = true;     
