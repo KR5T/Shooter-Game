fe03f9e5b8963c8e720250ae6aee57daa2b5c15e
KR5T <sakarya1869@gmail.com>
2026-02-02 16:55:59 +0300

    hit vfx

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public GameObject hitParticle;
     public Animator animator;
     public ParticleSystem particle; 
     StarterAssetsInputs inputs;
@@ -30,6 +31,8 @@ public class Weapon : MonoBehaviour
 
         if (Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out RaycastHit hit))                         
         {
+            Instantiate(hitParticle, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity);
+
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             
             if(enemyHealth)
