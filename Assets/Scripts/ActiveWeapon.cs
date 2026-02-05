5a9cd9eb6e6f1b6c6bdbe94e3163d98e7a718c6a
KR5T <sakarya1869@gmail.com>
2026-02-06 00:04:07 +0300

    Cooldown

--- Assets/Scripts/ActiveWeapon.cs
+++ Assets/Scripts/ActiveWeapon.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using StarterAssets;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ActiveWeapon : MonoBehaviour
 {
     public WeaponSO weaponSO;
     Animator animator;
-
     StarterAssetsInputs inputs;
     Weapon activeWeapon;
-
+    
     const string SHOOT_STRING = "Shoot";
+    float cooldownTime = 0f;
 
     void Awake()
     {
@@ -26,16 +27,18 @@ public class ActiveWeapon : MonoBehaviour
 
     void Update()
     {
+        cooldownTime += Time.deltaTime;
         HandleShoot();
     }
 
     void HandleShoot()
     {
-        if (!inputs.shoot) return;
-        activeWeapon.Shoot(weaponSO);
-        
-        animator.Play(SHOOT_STRING, 0, 0f);
+        if (!inputs.shoot ) return;
+        if(cooldownTime>=weaponSO.FireRate){
+            cooldownTime = 0f;
+            activeWeapon.Shoot(weaponSO);
+            animator.Play(SHOOT_STRING, 0, 0f);
+        }
         inputs.shoot = false;
     }
-        
 }
