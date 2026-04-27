9b8fb61ef682c0f8b9944d1034f51655a568915c
KR5T <sakarya1869@gmail.com>
2026-04-27 23:52:08 +0300

    Skeleton Death VFX

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public Animator animator;
+    public GameObject deathVFX;
     public int health = 100;
-    bool isDead = false;
 
     const string HIT_STRING = "Hit";
 
     public void TakeDamage(int damageTaken)
     {
+        Vector3 damagePosition = transform.position;
+        damagePosition.y = transform.position.y + 1f;
         animator.Play(HIT_STRING, 0, 0f);
         health -= damageTaken;
-        if(health <= 0) 
-            isDead = true;     
-        if(isDead) 
-            Destroy(this.gameObject);      
+        if(health <= 0)       
+            {
+                Instantiate(deathVFX, damagePosition, Quaternion.identity);
+                Destroy(this.gameObject);
+            }      
     }
 
 }
